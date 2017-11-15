0d50563c6fb1d89b5747357b9820159eb1b341c6
oneraul <oneraul@gmail.com>
2017-11-15 16:20:47 +0100

    Added From, To, Slope and Direction properties to the VisibilityRay class

--- VisibilityRay.cs
+++ VisibilityRay.cs
@@ -5,16 +5,19 @@ namespace MazeGamePillaPilla
 {
     class VisibilityRay : IIntersectable
     {
-        private Vector2 from;
-        private Vector2 to;
         private Rectangle aabb;
         private Vector2[] vertices;
         private Vector2[] projectionAxis;
 
+        public Vector2 From { get; private set; }
+        public Vector2 To { get; private set; }
+        public float Slope { get; private set; }
+        public Vector2 Direction { get; private set; }
+
         public VisibilityRay(Vector2 from, Vector2 to)
         {
-            this.from = from;
-            this.to = to;
+            this.From = from;
+            this.To = to;
 
             float x = Math.Min(from.X, to.X);
             float y = Math.Min(from.Y, to.Y);
@@ -27,6 +30,10 @@ namespace MazeGamePillaPilla
             Vector2 rayProjectionAxis = to - from;
             rayProjectionAxis.Normalize();
             projectionAxis = new Vector2[] { new Vector2(rayProjectionAxis.Y, -rayProjectionAxis.X) };
+
+            Slope = (to.Y-from.Y) / (to.X-from.X);
+            Direction = to - from;
+            Direction.Normalize();
         }
 
         public Vector2[] GetVertices()
